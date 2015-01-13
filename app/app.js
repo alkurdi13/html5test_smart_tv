0f09489ac580454af934a71c004d3ebbd1ee0e57
orangeetv <support@orangee.tv>
2015-01-13 14:46:25 -0500

    add keyboard navigation

--- app/app.js
+++ app/app.js
@@ -2,7 +2,24 @@
 'use strict';
 var app = new Orangee.Application();
 
+var InvisibleViewForKeyboard = Orangee.ItemView.extend({
+  template: false,
+  keyEvents: {
+    'up': 'onKeyUp',
+    'down': 'onKeyDown',
+  },
+  onKeyUp: function() {
+    orangee.log("onKeyUp");
+    window.scrollBy(0, -100);
+  },
+  onKeyDown: function() {
+    orangee.log("onKeyDown");
+    window.scrollBy(0, 100);
+  },
+});
+
 $(document).ready(function() {
   app.start();
+  new InvisibleViewForKeyboard().render();
 });
 
